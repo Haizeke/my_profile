6b6b0ee7e51a79e0735b32b27bae03fab99a7891
Haizeke <119109573+Haizeke@users.noreply.github.com>
2025-12-24 02:45:06 +0100

    Add motivational messages to setup.sh
    Added motivational messages to the setup script.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "Setup and configure server"
 
 file_name=config.yaml
-
+echo "I must make it in this DevOps thing"
+echo "The grind must pay off"
 
 echo "using file $file_name to configure something"
